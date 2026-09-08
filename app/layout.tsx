740e9d63ad59aca68dafb8bc803c9e90a6fc5cf1
Mugil Ms <mugilms7679@gmail.com>
2026-09-08 11:38:07 +0530

    feat: add Microsoft Clarity analytics and update Google search console verification tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,10 @@ export const metadata: Metadata = {
     apple: [{ url: "/images/idea%20logo.webp", type: "image/png" }]
   },
   verification: {
-    google: "IoTBz0cEobJ80992_poSZ0vrVo4Dk9AvCGJz9vjYQEw",
+    google: [
+      "mDTg1HB8JOYS8T6sn9VH29ktMqI4Ns1KYWJz6kxAupw",
+      "IoTBz0cEobJ80992_poSZ0vrVo4Dk9AvCGJz9vjYQEw"
+    ],
     other: {
       "msvalidate.01": ["8B36D5965F0BFE9929E6F42BFF5F3F97"]
     }
@@ -32,7 +35,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-scroll-behavior="smooth" suppressHydrationWarning>
-      <head />
+      <head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "yejo0eorsa");`
+          }}
+        />
+      </head>
       <body suppressHydrationWarning>
         <MetaPixel />
         <DisableImageActions />
